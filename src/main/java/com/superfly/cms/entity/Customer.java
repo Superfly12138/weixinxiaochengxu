75b2bfb97115a35da9d1dc49e62b31dc74784953
Super fly <903033214@qq.com>
2019-04-08 10:39:40 +0800

    second

--- src/main/java/com/superfly/cms/entity/Customer.java
+++ src/main/java/com/superfly/cms/entity/Customer.java
@@ -1,4 +1,87 @@
 package com.superfly.cms.entity;
 
+import java.util.Date;
+
 public class Customer {
+    private Integer cusId;
+    private String cusPassword;
+    private String cusName;
+    private String cusSex;
+    private Integer cusAge;
+    private String cusPhone;
+    private String cusAddress;
+    private String cusEmail;
+    private Date cusRegistrationDate;
+
+    public Integer getCusId() {
+        return cusId;
+    }
+
+    public void setCusId(Integer cusId) {
+        this.cusId = cusId;
+    }
+
+    public String getCusPassword() {
+        return cusPassword;
+    }
+
+    public void setCusPassword(String cusPassword) {
+        this.cusPassword = cusPassword;
+    }
+
+    public String getCusName() {
+        return cusName;
+    }
+
+    public void setCusName(String cusName) {
+        this.cusName = cusName;
+    }
+
+    public String getCusSex() {
+        return cusSex;
+    }
+
+    public void setCusSex(String cusSex) {
+        this.cusSex = cusSex;
+    }
+
+    public Integer getCusAge() {
+        return cusAge;
+    }
+
+    public void setCusAge(Integer cusAge) {
+        this.cusAge = cusAge;
+    }
+
+    public String getCusPhone() {
+        return cusPhone;
+    }
+
+    public void setCusPhone(String cusPhone) {
+        this.cusPhone = cusPhone;
+    }
+
+    public String getCusAddress() {
+        return cusAddress;
+    }
+
+    public void setCusAddress(String cusAddress) {
+        this.cusAddress = cusAddress;
+    }
+
+    public String getCusEmail() {
+        return cusEmail;
+    }
+
+    public void setCusEmail(String cusEmail) {
+        this.cusEmail = cusEmail;
+    }
+
+    public Date getCusRegistrationDate() {
+        return cusRegistrationDate;
+    }
+
+    public void setCusRegistrationDate(Date cusRegistrationDate) {
+        this.cusRegistrationDate = cusRegistrationDate;
+    }
 }
