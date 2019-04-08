cfc4462131b0853fe3d62f7e0479db71e69216e9
Super fly <903033214@qq.com>
2019-04-08 11:23:05 +0800

    CustomerDao mapper:CustomerDao

---
+++ src/main/java/com/superfly/cms/dao/CustomerDao.java
@@ -0,0 +1,41 @@
+package com.superfly.cms.dao;
+
+import com.superfly.cms.entity.Customer;
+
+import java.util.List;
+
+public interface CustomerDao {
+    /**
+     * 查询所有的顾客
+     * @return 顾客列表
+     */
+    List<Customer> queryCustomer();
+
+    /**
+     * @param cusId
+     * 通过Id查询顾客
+     * @return 单个顾客
+     */
+    Customer queryCustomerById(int cusId);
+
+    /**
+     * 插入顾客信息
+     * @param customer
+     * @return success:1 flase:0
+     */
+    int insertCustomer(Customer customer);
+
+    /**
+     * 更新顾客信息
+     * @param customer
+     * @return success:1 flase:0
+     */
+    int updateCustomer(Customer customer);
+
+    /**
+     * 通过传入的Id删除顾客列表
+     * @param customerId
+     * @return success:1 flase:0
+     */
+    int deleteCustomer(int customerId);
+}
